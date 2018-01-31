c5a5300a5b1a458361258538979244d39349e9ae
Matthew <2260469s@student.gla.ac.uk>
2018-01-31 16:12:33 +0000

    url mapping

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
 	category_list = Category.objects.order_by("-likes")[:5]
@@ -11,3 +12,16 @@ def about(request):
 	context_dict = {'your_name': "Matthew"}
 	return render(request, 'rango/about.html', context=context_dict)
 
+def show_category(request, category_name_slug):
+        context_dict = {}
+
+        try:
+                category = Category = Category.objects.get(slug=category_name_slug)
+                pages = Page.objects.filter(category=category)
+                context_dict['pages'] = pages
+                context_dict['category'] = category
+        except Category.DoesNotExist:
+                context_dict['pages'] = None
+                context_dict['category'] = None
+
+        return render(request, 'rango/category.html', context_dict)
