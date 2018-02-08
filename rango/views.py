98b49b85afd7acaede715cc60eb7bfb8e89e147e
Matthew <2260469s@student.gla.ac.uk>
2018-02-08 18:59:45 +0000

    template inheritance

--- rango/views.py
+++ rango/views.py
@@ -36,7 +36,7 @@ def add_category(request):
                 form = CategoryForm(request.POST)
 
                 if form.is_valid():
-                        form.save(commit=TRUE)
+                        form.save(commit=True)
                         return index(request)
                 else:
                         print(form.errors)
