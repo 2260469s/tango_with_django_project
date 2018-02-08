a1a516c8e845d3027024997ec3344faf664cd2df
Matthew <2260469s@student.gla.ac.uk>
2018-02-08 22:09:36 +0000

    users can login

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,9 @@ from rango.models import Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 
 def index(request):
 	category_list = Category.objects.order_by("-likes")[:5]
@@ -96,3 +99,25 @@ def register(request):
 		profile_form = UserProfileForm()
 	
 	return render(request, 'rango/register.html', {'user_form': user_form, 'profile_form':profile_form, 'registered':registered})
+
+def user_login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		
+		user = authenticate(username=username, password=password)
+		
+		if user:
+		
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect(reverse('index'))
+			else:
+				return HttpResponse("Your Rango account is disabled.")
+			
+		else:
+			print("Invalid login details: {0}, {1}".format(username, password))
+			return HttpResponse("Invalid login details supplied.")
+	else:
+		return render(request, 'rango/login.html', {})
+		
