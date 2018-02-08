1cdf03f932f0eacec85a29e07376940a91511dd1
Matthew <2260469s@student.gla.ac.uk>
2018-02-08 21:45:26 +0000

    can now register

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name_length = 128
@@ -12,11 +13,11 @@ class Category(models.Model):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 	
-        class Meta:
-            verbose_name_plural = "Categories"
+    class Meta:
+        verbose_name_plural = "Categories"
 	
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
 		
 class Page(models.Model):
     title_length = 128
@@ -28,3 +29,11 @@ class Page(models.Model):
 	
     def __str__(self):
         return self.title
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+	
+	def __str__(self):
+		return self.user.username
